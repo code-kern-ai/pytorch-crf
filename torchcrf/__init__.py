e384e41d0e93ed795f82b53937a255cb70b4e6e3
Johannes Hötter <jhoetter@MBPvonJohannes.fritz.box>
2022-05-06 01:15:12 +0200

    add confidence to viterbi decode

--- torchcrf/__init__.py
+++ torchcrf/__init__.py
@@ -117,26 +117,33 @@ class CRF(nn.Module):
     def decode(self, emissions: torch.Tensor,
                mask: Optional[torch.ByteTensor] = None) -> List[List[int]]:
         """Find the most likely tag sequence using Viterbi algorithm.
-
         Args:
             emissions (`~torch.Tensor`): Emission score tensor of size
                 ``(seq_length, batch_size, num_tags)`` if ``batch_first`` is ``False``,
-                ``(batch_size, seq_length, num_tags)`` otherwise.
+                ``(batch_size, seq_length, num_tags)`` otherwise.                
             mask (`~torch.ByteTensor`): Mask tensor of size ``(seq_length, batch_size)``
                 if ``batch_first`` is ``False``, ``(batch_size, seq_length)`` otherwise.
-
         Returns:
             List of list containing the best tag sequence for each batch.
         """
         self._validate(emissions, mask=mask)
         if mask is None:
             mask = emissions.new_ones(emissions.shape[:2], dtype=torch.uint8)
+      
+        # Find the optimal sequence
+        best_tag_sequence = self._viterbi_decode(emissions, mask)
+        best_tag_sequence = torch.tensor(best_tag_sequence)
+        best_tag_sequence = best_tag_sequence.transpose(0, 1)
 
-        if self.batch_first:
-            emissions = emissions.transpose(0, 1)
-            mask = mask.transpose(0, 1)
-
-        return self._viterbi_decode(emissions, mask)
+        # Find the probability of the optimal sequence
+        denominator = self._compute_normalizer(emissions, mask)
+        numerator = self._compute_score(emissions, best_tag_sequence, mask)
+        llh = numerator - denominator
+        log_likelihood = llh.sum() / mask.float().sum()
+    
+        confidence = torch.exp(log_likelihood)
+        
+        return self._viterbi_decode(emissions, mask), confidence
 
     def _validate(
             self,
